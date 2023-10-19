cf6c9819de4cf283520fd8f730e9dc29510f4660
Putrinadia23 <116242328+Putrinadia23@users.noreply.github.com>
2023-10-19 21:07:41 +0700

    Add files via upload

--- latihan3.php
+++ latihan3.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hasil Perhitungan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7Rxnatzjc-dSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <?php
     if (isset($_GET['angka']) && isset($_GET['hasil'])) {
         $angka = $_GET['angka'];
         $hasil = $_GET['hasil'];
-        echo "<h1>Hasil perhitungan untuk angka $angka adalah: $hasil</h1>";
+        echo "<h1 class='text-center font-weight-bold small'>Hasil perhitungan untuk angka $angka adalah: $hasil</h1>";
     } else {
-        echo "<h1>Hasil tidak tersedia.</h1>";
+        echo "<h1 class='text-center font-weight-bold small'>Hasil tidak tersedia.</h1>";
     }
     ?>
 </body>
 </html>
-
